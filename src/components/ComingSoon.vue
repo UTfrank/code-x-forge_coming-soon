b19f197474a48ffbce9547429296694719b2bc54
Frank John <frankjoe261@gmail.com>
2025-04-26 19:20:36 +0100

    feat: implement coming soon component

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="h-[89vh] flex justify-center items-center">
+    <h1 class="text-[#1D252C] text-[120px] font-bold uppercase w-11/12 !ml-auto !mr-auto">Something Awesome is coming...</h1>
+  </section>
+</template>
+
+<style scoped>
+  
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="w-11/12 !ml-auto !mr-auto py-3.5 flex justify-between items-center">
+    <RouterLink to="/">
+      <img class="h-14 w-14 rounded-full" src="/src/assets/logo-1.jpg" alt="logo"
+    /></RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
