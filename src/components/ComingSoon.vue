67be4b32b6e53df41a5cefc071672bab4f8362ac
Frank John <frankjoe261@gmail.com>
2025-04-30 10:39:48 +0100

    chore: implement changes to coming soon component

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -1,12 +1,16 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <section class="h-[89vh] flex justify-center items-center">
-    <h1 class="text-[#1D252C] text-[120px] font-bold uppercase w-11/12 !ml-auto !mr-auto">Something Awesome is coming...</h1>
+  <section class="h-[89vh] w-full pt-40 flex justify-center items-start">
+    <div class="flex flex-col gap-y-2 text-center text-[#1D252C] lg:w-8/12 w-10/12">
+      <h1 class="md:text-[100px] text-7xl font-black uppercase !ml-auto !mr-auto">
+      Coming Soon...
+      </h1>
+        <p class="text-lg">
+        From automation of people processes to creating an engaged and driven culture. 
+      </p>
+    </div>
   </section>
 </template>
 
-<style scoped>
-  
-</style>
+<style scoped></style>
